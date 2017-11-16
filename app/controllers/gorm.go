fbf86182ed518261ee01c1206c754f4da49456e5
yatuhashi kei <keyansuiya@gmail.com>
2017-11-17 02:13:12 +0900

    first commit

---
+++ app/controllers/gorm.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	// _ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/revel/revel"
+	"github.com/yatuhashi/base/app/models"
+	"log"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	db, err := gorm.Open("sqlite3", dbInfoString())
+	if err != nil {
+		log.Panicf("Failed to connect to database: %v\n", err)
+	}
+
+	db.DB()
+	db.AutoMigrate(&models.User{})
+	DB = &db
+}
+
+func dbInfoString() string {
+	s, b := revel.Config.String("db.info")
+	if !b {
+		log.Panicf("database info not found")
+	}
+	return s
+}
---
+++ app/models/user.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+)
+
+type Model struct {
+	ID        uint64    `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type User struct {
+	gorm.Model
+	Username string `sql:"size:64" json:"username" validate:"max=64"`
+	Password string `sql:"size:64" json:"password" validate:"max=64"`
+	Role     string `sql:"size:32" json:"role" validate:"max=32"`
+}
